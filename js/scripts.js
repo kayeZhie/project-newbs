7335efe331d5bbb0e845e929ef79c22b0b98763f
Jon Maldia <jon@maldia.net>
2018-04-06 16:26:17 -0400

    Fixes slideshow carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,50 @@
-*
-var slideIndex = 1;
-showSlides(slideIndex);
+///////////////////////
+// CODE FOR CAROUSEL //
+///////////////////////
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+$("#slideshow-container > div:gt(0)").hide();
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+setInterval(function() { 
+  $('#slideshow-container > div:first')
+    .fadeOut(0)
+    .next()
+    .fadeIn(1200)
+    .end()
+    .appendTo('#slideshow-container');
+},  5000);
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("main-header");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+
+
+///////////////////////////
+// OLD CODE FOR CAROUSEL //
+///////////////////////////
+
+// var slideIndex = 1;
+// // showSlides(slideIndex);
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("main-header");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";  
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace("active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+// }
+
+// setInterval(showSlides(slideIndex), 3000);
 
